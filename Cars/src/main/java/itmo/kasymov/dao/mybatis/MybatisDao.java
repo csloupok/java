87043b11c9383a609fee797557a432132dfa011c
eldarkasymov <csloupok@gmail.com>
2023-04-01 12:21:40 +0300

    fix: remove commments

--- Cars/src/main/java/itmo/kasymov/dao/mybatis/MybatisDao.java
+++ Cars/src/main/java/itmo/kasymov/dao/mybatis/MybatisDao.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package itmo.kasymov.dao.mybatis;
 
 import org.apache.ibatis.io.Resources;
